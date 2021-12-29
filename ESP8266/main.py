5478da06e001b9d559e67f8420f9299cc8321925
RenzoVigiani <vigiani.renzo.l@gmail.com>
2021-12-29 14:32:16 -0300

    pruebas con funciones

--- ESP8266/main.py
+++ ESP8266/main.py
@@ -1,9 +1,14 @@
 import machine
 import time
 
-def toggle(p):
-   p.value(not p.value())
-   
+
+# Declaración de variables   
+val_eeprom = 64
+datos_eeprom = [val_eeprom]
+temp=[val_eeprom]
+datoTAG=[val_eeprom]
+
+
 # ----- SALIDAS ---------
 Led_Verde_grande = machine.Pin(15, machine.Pin.OUT)
 Led_Verde_chico = machine.Pin(16, machine.Pin.OUT)
@@ -17,25 +22,44 @@ Pin_MOSI = machine.Pin(13)
 Pin_SCLK = machine.Pin(14)
 Pin_SDA  = machine.Pin(4)
 
-# -------- SERIAL --------------
-'''Pin_Flash = machine.Pin(0)
-Pin_TXD0 = machine.Pin(1, machine.Pin.OUT)
-Pin_RXD0 = machine.Pin(3, machine.Pin.IN)
-'''
-Led_Verde_chico.off()
-Led_Verde_grande.off()
-Led_senal_rojo.on()
-time.sleep_ms(500)        
-Led_senal_verde.off()
-time.sleep_ms(500)        
+# Definición de funciones
+#------ toggle ----------
+def toggle(p):
+   p.value(not p.value())
+# ------- tagOK ------
+def tagOK():
+    Led_Verde_chico.on()
+    Buzzer.on()
+    time.sleep_ms(200)
+    Buzzer.off()
+    Led_Verde_chico.off()
 
-for i in range(0,10):
-    if i==10:
-        break
-    toggle(Led_senal_rojo)
-    toggle(Led_Verde_chico)
-    toggle(Led_Verde_grande)
-    time.sleep_ms(500)        
-Buzzer.on()
-time.sleep_ms(100)        
-Buzzer.off()
+# ------- tagERROR ------
+def tagERROR():
+    Led_senal_rojo.on() # led rojo apagado
+    Buzzer.off() # Buzzer apagado
+    for i in range(6): # hacer titilar 3 veces rapido
+        toggle(Led_senal_rojo)
+        toggle(Buzzer)
+        time.sleep_ms(150)
+
+# ------ initBuffer -----
+def initBuffer():
+    for i in range(val_eeprom): # val_eeprom = 64
+        datos_eeprom[i] = 0
+        temp[i] = 0
+        datoTAG[i]=0
+# ------ clearBuffData----
+def clearBuffData():
+    for i in range(val_eeprom):
+        datoTAG[i]=0        
+
+
+# Main
+for i in range (0,100,5):
+    if i==35:
+        tagERROR()
+    if i==90:
+        tagOK
+    print(i)
+    time.sleep_ms(100)
