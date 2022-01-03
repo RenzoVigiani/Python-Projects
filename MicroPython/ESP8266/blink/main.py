28909c98152cff5564e183dbd2c57e8989d2227c
RenzoVigiani <vigiani.renzo.l@gmail.com>
2022-01-03 08:34:43 -0300

    actualización Mycropython

---
+++ MicroPython/ESP8266/blink/main.py
@@ -0,0 +1,45 @@
+# Complete project details at https://RandomNerdTutorials.com
+from machine import Pin
+import time
+
+def toggle(p):
+       p.value(not p.value())
+# Configurar GPIO
+#RST_PIN = Pin(30, Pin.IN) # RST-PIN für RC522 - RFID - SPI - Modul GPIO5
+#SS_PIN = Pin(4, Pin.OUT) # SDA-PIN für RC522 - RFID - SPI - Modul GPIO4  4
+
+OFFSET_ZERO = 0
+OFFSET_16 = 16 
+OFFSET_32 = 32
+OFFSET_48 = 48
+#define MIFARECLASSIC 1
+#define NTAG21x 2
+#define KEY_A  1
+
+BUZZER = Pin(5,Pin.OUT)
+LEDVERDE = Pin(16,Pin.OUT) #Logica positiva
+LEDSENALROJO = Pin(2,Pin.OUT) # lógica negativa
+LEDSENALVERDE = Pin(5,Pin.OUT) #Logica negativa
+LEDSENALVERDEGRANDE = Pin(15,Pin.OUT) #Logica positiva
+
+i=0
+while i<22:
+  if i%2==0:
+#    LEDSENALVERDE.off()
+    LEDSENALROJO.off()
+    LEDSENALVERDEGRANDE.on()
+    LEDVERDE.on()
+    BUZZER.off()
+  if i%2!=0:
+#    LEDSENALVERDE.off()
+    LEDSENALROJO.on()
+    LEDVERDE.off()
+    LEDSENALVERDEGRANDE.off()
+#    BUZZER.on()
+  time.sleep_ms(500)
+  i+=1
+  if i==22:
+    print(i)
+    LEDSENALVERDE.on()
+    time.sleep_ms(1000)
+    LEDSENALVERDE.off()
